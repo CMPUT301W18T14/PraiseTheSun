361de579796c87043a38f9889bece7e4dedbc547
cmbuhler <cmbuhler@ualberta.ca>
2018-02-23 17:35:52 -0700

    Added task class

--- app/src/main/java/ca/ualbert/cs/tasko/Task.java
+++ app/src/main/java/ca/ualbert/cs/tasko/Task.java
@@ -15,9 +15,39 @@
 
 package ca.ualbert.cs.tasko;
 
+import android.location.Location;
+import android.media.Image;
+
+import java.util.ArrayList;
+
 /**
  * Created by Chase on 2/23/2018.
  */
 
 public class Task {
+
+    private String taskName;
+    private String description;
+    private ArrayList<Image> photos;
+    private Location geolocation;
+    private User taskRequester;
+    private ArrayList<Bid> bidList;
+    private Status status;
+
+    public Task(User taskRequester, String taskName, String description){
+        this(taskRequester, taskName, description, null, null)
+    }
+
+    public Task(User taskRequester, String taskName, String description, ArrayList<Image> photos){
+        this(taskRequester, taskName, description, photos, null);
+    }
+
+    public Task(User taskRequester, String taskName, String description, Location location){
+        this(taskRequester, taskName, description, null, location);
+    }
+
+    public Task(User taskRequester, String taskName, String description, ArrayList<Image> photos,
+                Location location){
+
+    }
 }
