665263d2643355539d654f5cfc2853b031af970b
ryandromano <rromano@ualberta.ca>
2018-03-07 01:39:42 -0700

    ViewMyTasks and TaskDetail xml layouts

--- app/src/main/java/ca/ualbert/cs/tasko/ViewMyTasksActivity.java
+++ app/src/main/java/ca/ualbert/cs/tasko/ViewMyTasksActivity.java
@@ -15,6 +15,7 @@
 
 package ca.ualbert.cs.tasko;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,8 +24,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class ViewMyTasks extends AppCompatActivity {
+public class ViewMyTasksActivity extends AppCompatActivity {
+
+    public ListView myTaskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,19 @@ public class ViewMyTasks extends AppCompatActivity {
         setContentView(R.layout.activity_view_my_tasks);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        myTaskList = (ListView) findViewById(R.id.taskListView);
+
+        myTaskList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                startActivity(new Intent(ViewMyTasksActivity.this, ViewTaskDetailsActivity.class));
+            }
+        });
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
