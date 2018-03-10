d615c10361b5a292c9971cabda4cb66857d43945
tlafranc <tlafranc@ualberta.ca>
2018-03-10 14:06:14 -0700

    Changed query for GetUserBids

--- app/src/main/java/ca/ualbert/cs/tasko/Commands/DataCommands/GetUserBidsCommand.java
+++ app/src/main/java/ca/ualbert/cs/tasko/Commands/DataCommands/GetUserBidsCommand.java
@@ -45,7 +45,7 @@ public class GetUserBidsCommand extends GetCommand<BidList> {
 
     @Override
     public void execute() {
-        String query = "{\"query\":{\"match\":{\"UserID\":\"" + id + "\" } } }";
+        String query = "{\"query\":{\"term\":{\"UserID\":\"" + id + "\" } } }";
         GetBidListTask getBidListTask = new GetBidListTask();
         getBidListTask.execute(query);
         try {
