105930e8a43dc3fbd809e4ed58ccae8ec28acae2
Alden Emerson Ern Tan <aet@ualberta.ca>
2018-03-12 04:03:00 -0600

    did git add -A, not committing before push to alden branch

---
+++ app/src/main/java/ca/ualbert/cs/tasko/ViewBidsOnTaskActivity.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2018 Chase Buhler, Imtihan Ahmed, Thomas Lafrance, Ryan Romano, Stephen Packer,
+ * Alden Emerson Ern Tan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.ualbert.cs.tasko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * Created by Aldentan1997 on 2018-03-12.
+ */
+
+    //Approprite xml file "activity_view_bids_on_task.xml" -the toolbar cuts into the listview
+    //Need to fix ^
+
+    //TODO: MAKE TEST FOR THIS AND APPROPRIATE XML FILES!
+
+public class ViewBidsOnTaskActivity extends AppCompatActivity {
+    public ListView myBidList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_bids_on_task);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        myBidList = (ListView) findViewById(R.id.bidListView);
+
+        /*TODO:
+        In storyboard each bid in the list of bids has 2 buttons: one for Accept and Reject.
+        I don't know how to make it so that each bid item in the list has those 2 buttons... */
+
+        //Not sure what this is for - need to find out
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+
+    //Taken from ViewMyTasksActivity
+    //Find out what they do and whether this activity needs it
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
