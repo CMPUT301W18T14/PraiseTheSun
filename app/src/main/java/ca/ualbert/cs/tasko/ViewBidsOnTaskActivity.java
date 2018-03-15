ca10e0f59a8c0f297cbba8495e117005417526f3
Alden Emerson Ern Tan <aet@ualberta.ca>
2018-03-14 20:35:22 -0600

    populating listview with objects

--- app/src/main/java/ca/ualbert/cs/tasko/ViewBidsOnTaskActivity.java
+++ app/src/main/java/ca/ualbert/cs/tasko/ViewBidsOnTaskActivity.java
@@ -23,7 +23,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Aldentan1997 on 2018-03-12.
@@ -42,6 +46,26 @@ public class ViewBidsOnTaskActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         myBidList = (ListView) findViewById(R.id.bidListView);
+
+        //Populating za listview with some stuffzzzzz
+
+        // Instanciating an array list (you don't need to do this,
+        // you already have yours).
+        List<String> your_array_list = new ArrayList<String>();
+        your_array_list.add("foo");
+        your_array_list.add("bar");
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+
+        myBidList.setAdapter(arrayAdapter);
+
+        //end of listview population codezzzzz
 
         /*TODO:
         In storyboard each bid in the list of bids has 2 buttons: one for Accept and Reject.
