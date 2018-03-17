8c6c6971f6c1808d79f8f32d346126c0cac36cfa
ryandromano <rromano@ualberta.ca>
2018-03-17 17:23:09 -0600

    Somewhat working display

--- app/src/main/java/ca/ualbert/cs/tasko/ViewMyTasksActivity.java
+++ app/src/main/java/ca/ualbert/cs/tasko/ViewMyTasksActivity.java
@@ -21,6 +21,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -29,8 +31,11 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class ViewMyTasksActivity extends RootActivity {
+import ca.ualbert.cs.tasko.data.DataManager;
+import ca.ualbert.cs.tasko.data.NoInternetException;
 
+public class ViewMyTasksActivity extends RootActivity {
+    /*
     public ListView myTaskList;
     private Integer statusFilter;
 
@@ -74,6 +79,40 @@ public class ViewMyTasksActivity extends RootActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+}
+*/
+    private RecyclerView searchRecyclerView;
+    private RecyclerView.Adapter searchAdapter;
+    private RecyclerView.LayoutManager searchLayoutManager;
+    private DataManager dm = DataManager.getInstance();
+    private ViewMyTasksActivity activity = this;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+
+        searchRecyclerView = (RecyclerView) findViewById(R.id.search_task_recycler_view);
+
+        searchLayoutManager = new LinearLayoutManager(activity);
+        searchRecyclerView.setLayoutManager(searchLayoutManager);
+
+        Bundle extras = getIntent().getExtras();
+
+        TaskList myTasks = new TaskList();
+
+        try {
+            myTasks = dm.getUserTasks(CurrentUser.getInstance().getCurrentUser().getId(), activity);
+        } catch (NoInternetException e) {
+            e.printStackTrace();
+        }
+
+
+        searchAdapter = new TaskListAdapter(activity, myTasks);
+        searchRecyclerView.setAdapter(searchAdapter);
     }
 
 }
