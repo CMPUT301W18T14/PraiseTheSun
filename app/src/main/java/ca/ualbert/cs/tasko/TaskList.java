2ab22214f98764023670d449722186e4d2c74abe
Stephen Packer <spacker@ualberta.ca>
2018-03-18 23:06:59 -0600

    Adding TaskList, TaskListAdapter and CurrentUser Documentation

--- app/src/main/java/ca/ualbert/cs/tasko/TaskList.java
+++ app/src/main/java/ca/ualbert/cs/tasko/TaskList.java
@@ -20,36 +20,70 @@ import java.util.Collection;
 
 /**
  * Created by Thomas on 2018-02-23.
- * Represents a TaskList object that contains an array of tasks.
+ * Represents a TaskList object that contains an array of tasks. Includes the ability to
+ * add/ remove tasks as well as getting tasks at specific indices or returning the entire
+ * ArrayList.
+ * @see Task
  *
  * @author tlafranc
  */
 public class TaskList {
 
+    /**
+     * Initializes The TaskList as an ArrayList of Task objects.
+     */
     private ArrayList<Task> tasks = new ArrayList<Task>();
 
+    /**
+     * Default Constructor.
+     */
     public TaskList(){}
 
+    /**
+     * Adds a Task to the TaskList.
+     * @param task a Task object.
+     */
     public void addTask(Task task){
         tasks.add(task);
     }
 
+    /**
+     * Removes a specific Task from the TaskList
+     * @param task the Task that is requested to be removed.
+     */
     public void removeTask(Task task){
         tasks.remove(task);
     }
 
+    /**
+     * Get all the Tasks from a TaskList.
+     * @return An ArrayList containing all Task objects stored in the TaskList.
+     */
     public ArrayList<Task> getTasks(){
         return tasks;
     }
 
+    /**
+     *
+     * @param c
+     */
     public void addAll(Collection<? extends Task> c){
         tasks.addAll(c);
     }
 
+    /**
+     * Returns the size of the TaskList (How many tasks it contains) Needed for RecyclerView Adapter
+     * @return An integer representing the size of the TaskList
+     */
     public int getSize(){
         return tasks.size();
     }
 
+    /**
+     * Returns the Task found at a specific position in the TaskList.
+     * @param position The interger position of the Task we wish to return.
+     * @return a Task object.
+     */
     public Task get(int position){
         return tasks.get(position);
     }
