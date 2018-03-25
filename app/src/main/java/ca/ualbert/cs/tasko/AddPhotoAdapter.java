cb05c7de3bdda43b20aabf033875694224d8fa22
tlafranc <tlafranc@ualberta.ca>
2018-03-25 16:51:26 -0600

    Changed AddPhotoActivity so now you are able to select multiple photos at once. Trying to implement a RecyclerView that would display all the images.

---
+++ app/src/main/java/ca/ualbert/cs/tasko/AddPhotoAdapter.java
@@ -0,0 +1,10 @@
+package ca.ualbert.cs.tasko;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Thomas on 2018-03-25.
+ */
+
+public class AddPhotoAdapter extends RecyclerView.Adapter {
+}
