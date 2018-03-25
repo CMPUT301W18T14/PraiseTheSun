ed482e772e2b728813e33b982534f801308cd30e
Imtihan Ahmed <imtihan@ualberta.ca>
2018-03-24 23:36:27 -0600

    made a new better user profile page

--- app/src/main/java/ca/ualbert/cs/tasko/UserActivity.java
+++ app/src/main/java/ca/ualbert/cs/tasko/UserActivity.java
@@ -15,8 +15,16 @@
 
 package ca.ualbert.cs.tasko;
 
+import android.content.Intent;
+import android.provider.DocumentsContract;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * UserActivity shows the user's information. Accessed from the menu
@@ -24,11 +32,67 @@ import android.os.Bundle;
  * @author imtihan
  * @see RootActivity, User
  */
-public class UserActivity extends AppCompatActivity {
-
+public class UserActivity extends RootActivity {
+    TextView username;
+    TextView email;
+    TextView phone;
+    Button myTasks;
+    Button myAssignments;
+    Button editProfile;
+    private UserActivity activity = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.getMenu().getItem(0).setChecked(true);
         setContentView(R.layout.activity_user);
+
+        username = (TextView)findViewById(R.id.UserActivityUsername);
+        email = (TextView) findViewById(R.id.UserActivityUserEmail);
+        phone = (TextView) findViewById(R.id.UserActivityPhone);
+
+        username.setText(CurrentUser.getInstance().getCurrentUser().getUsername());
+        email.setText(CurrentUser.getInstance().getCurrentUser().getEmail());
+        phone.setText(CurrentUser.getInstance().getCurrentUser().getPhoneNumber());
+
+        myTasks = (Button) findViewById(R.id.UserActivityMyTasksButton);
+        myAssignments = (Button) findViewById(R.id.UserActivityMyAssignmentButton);
+        editProfile = (Button) findViewById(R.id.UserActivityEditProfileButton);
+
+        myTasksButton();
     }
+
+    public void myTasksButton(){
+        myTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                Intent intent = new Intent(activity, ViewMyTasksActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void myAssignmentsButton(){
+        //TODO when the activity is done
+        myAssignments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                //Intent intent = new Intent(activity, ViewMyAssignme)
+            }
+        });
+    }
+
+    public void editProfileButton(){
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                Intent intent = new Intent(activity,UserProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
