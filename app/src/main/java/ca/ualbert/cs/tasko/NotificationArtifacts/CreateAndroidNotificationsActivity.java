263eb933245aa0269433c41c50faec74d8d163c2
Stephen Packer <spacker@ualberta.ca>
2018-03-26 14:46:48 -0600

    Clickable android notifications are working!

--- app/src/main/java/ca/ualbert/cs/tasko/NotificationArtifacts/CreateAndroidNotificationsActivity.java
+++ app/src/main/java/ca/ualbert/cs/tasko/NotificationArtifacts/CreateAndroidNotificationsActivity.java
@@ -17,17 +17,31 @@ package ca.ualbert.cs.tasko.NotificationArtifacts;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.RemoteViews;
+import android.view.View;
+import android.widget.Button;
 
+import ca.ualbert.cs.tasko.MainActivity;
 import ca.ualbert.cs.tasko.R;
 
-public class AndroidNotificationActivity extends AppCompatActivity {
+/**
+ * This activity will be called when the DataManger finds a New Notification message for a user.
+ * When this Activity is created, It creates an Android Notification that when clicked will send the
+ * user to the ViewNotifications Activity where all their Notifications can be seen. his notifys
+ * the User that something has happened in Tasko, without this, A user outside of the app would never
+ * know if something has occured without physically navigating to the ViewMyNotifications Activity.
+ *@see Notification
+ *@see ViewNotificationActivity
+ *
+ * @author spack
+ */
+public class CreateAndroidNotificationsActivity extends AppCompatActivity {
 
     private NotificationCompat.Builder notificationBuilder;
     private NotificationManager notificationManager;
@@ -35,9 +49,14 @@ public class AndroidNotificationActivity extends AppCompatActivity {
     private Context context;
     private String CHANNEL_ID;
 
+    private Button testButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_create_notifications);
+
+        testButton = (Button) findViewById(R.id.notificationCreateButton);
 
         context = this;
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
@@ -46,24 +65,49 @@ public class AndroidNotificationActivity extends AppCompatActivity {
         CHANNEL_ID = getPackageName();
 
         setupChannel();
-        createAndriodNotification();
+        setupButton();
+
+        finish();
     }
 
+    //Just used for testing, when actually implemented their will be no associated layout with
+    //the activity
+    private void setupButton(){
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(RESULT_OK);
+                createAndriodNotification();
+            }
+        });
+    }
+
+    /**
+     * Setups the Channel we will use to send notifications... If performance is an issue, I will
+     * store the channel creation in a diffrent class so it does not need to be done everytime!
+     */
     private void setupChannel(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String name = "MyChannel";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.enableLights(true);
             notificationManager.createNotificationChannel(channel);
         }
     }
 
+    /**
+     * Creates the Android Notification that will go to the Users Device.
+     */
     private void createAndriodNotification(){
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, 0);
+
         notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID);
-        notificationBuilder.setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("Test Title")
-                .setContentText("You have New Notifications in Tasko")
+        notificationBuilder.setSmallIcon(R.drawable.ic_info_outline_black_24dp)
+                .setContentTitle("Tasko")
+                .setContentText("You have New Notifications in Tasko!")
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
 
         notificationManager.notify(notificationID, notificationBuilder.build());
