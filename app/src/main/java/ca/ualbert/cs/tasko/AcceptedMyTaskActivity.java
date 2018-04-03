e19a3f2a242082b40fc4683091175ea29f0b8380
ryandromano <rromano@ualberta.ca>
2018-04-03 00:23:25 -0600

    Activity setup

--- app/src/main/java/ca/ualbert/cs/tasko/AcceptedMyTaskActivity.java
+++ app/src/main/java/ca/ualbert/cs/tasko/AcceptedMyTaskActivity.java
@@ -15,14 +15,97 @@
 
 package ca.ualbert.cs.tasko;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import ca.ualbert.cs.tasko.data.DataManager;
+import ca.ualbert.cs.tasko.data.NoInternetException;
 
 public class AcceptedMyTaskActivity extends AppCompatActivity {
+    private TextView assignedTaskDescription;
+    private TextView assignedTaskName;
+    private TextView assignedTaskStatus;
+    private Task assignedCurrentTask;
+    private Button repostButton;
+    private Button completedButton;
+    private final Context context = this;
+    private DataManager dm = DataManager.getInstance();
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accepted_task);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Button and TextView definitions
+        repostButton = (Button) findViewById(R.id.repostButton);
+        completedButton = (Button) findViewById(R.id.taskCompleteButton);
+        assignedTaskName = (TextView) findViewById(R.id.assignedTaskName);
+        assignedTaskDescription = (TextView) findViewById(R.id.assignedTaskDescription);
+        assignedTaskStatus = (TextView) findViewById(R.id.assignedTaskStatusAndProvider);
+
+        Bundle extras = getIntent().getExtras();
+
+        try {
+            String taskID = extras.getString("TaskID");
+            assignedCurrentTask = dm.getTask(taskID, this);
+            fillInformation();
+        } catch (NullPointerException e) {
+            Log.i("Error", "TaskID not properly passed");
+        } catch (NoInternetException e) {
+            e.printStackTrace();
+        }
+
+        ImageView imageView = (ImageView) findViewById(R.id.myTasksImageView);
+        if (assignedCurrentTask.hasPhoto()) {
+            imageView.setImageBitmap(assignedCurrentTask.getCoverPhoto());
+        }
+
+        setupCompleteButton();
+
+        setupRepostButton();
+    }
+
+    private void fillInformation() {
+        assignedTaskName.setText(assignedCurrentTask.getTaskName());
+        assignedTaskDescription.setText(assignedCurrentTask.getDescription());
+        assignedTaskStatus.setText(assignedCurrentTask.getStatus().toString() + ": bid of $" +
+                assignedCurrentTask.getMinBid().toString() + "by " + assignedCurrentTask.getMinBid());
+    }
+
+    public void onPhotoClick(View view) {
+        Intent viewPhotosIntent = new Intent(this, ViewPhotoActivity.class);
+        viewPhotosIntent.putExtra("photos", assignedCurrentTask);
+        startActivity(viewPhotosIntent);
+    }
+
+    private void setupCompleteButton() {
+        completedButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                assignedCurrentTask.setStatus(Status.DONE);
+                finish();
+            }
+        });
+    }
+
+    private void setupRepostButton() {
+        repostButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Remove currently set bid and repost the task
+                //Unhide the previous bids that were on the task and notify the
+            }
+        });
+    }
 }
